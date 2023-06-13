b72be236e0e3e3f231cd294803f4c80444cf510a
Slawek Derabin <slawomirderabin@gmail.com>
2023-06-13 14:19:35 +0200

    add app

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+const multer = require('multer');
+
+const app = express();
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '/uploads'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { layout: 'dark' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.post('/contact/send-message', upload.single('design'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message) {
+    const fileName = req.file.originalname;
+    const fileExtension = path.extname(fileName).substring(1);
+
+    res.render('contact', { isSent: true, fileName, fileExtension });
+  } else {
+    res.render('contact', { isError: true });
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
